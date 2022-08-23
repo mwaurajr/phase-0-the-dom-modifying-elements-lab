6eb87ffdcb9289c60ce57bf220c043a8f21fcfd1
Frankiline <frankiline.mwaura@student.moringaschool.com>
2022-08-23 19:09:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+// Write your code here! 
 // Write your code here!
+//Remove main div
+const main = document.getElementById('main');
+main.remove();
+
+
+//Create new heading element with ID atttribute
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = 'Frankiline is the champion';
+
+main.appendChild(newHeader); 
